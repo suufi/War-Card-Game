85f4eea91c8c9158adb81cfe4a4061d0b87b6c02
Suufi <qxest@reqxest.com>
2018-05-01 23:47:41 -0400

    Attempt to handle hand sizes
    Needs more working however

--- src/com/suufi/war/client/ClientConnection.java
+++ src/com/suufi/war/client/ClientConnection.java
@@ -2,6 +2,7 @@ package com.suufi.war.client;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class ClientConnection extends Thread {
 	
@@ -10,8 +11,10 @@ public class ClientConnection extends Thread {
 	private PrintWriter out;   // what is sent to the server
 	private ClientGUI clientGUI;
 	private boolean isTurn;
+	private int oppHandSize;
 	
 	private Deck hand = new Deck();
+	
 	
 	public ClientConnection(String host) throws IOException {
 		this.socket = new Socket(host, 9009);
@@ -32,43 +35,98 @@ public class ClientConnection extends Thread {
 				if (data[0].equals("deal")) {
 					data = data[1].split("(?<=\\d)(?=\\D)");
 					
-					System.out.println(data[0]);
-					System.out.println(data[1]);
-					
-					String cardId = data[0] + data[1] + ".gif";
-					clientGUI.log(cardId);
+					String cardId = data[0] + data[1] + ".png";
+					// clientGUI.log(cardId);
 					
 					hand.addCard(data[0] + data[1]);
 					// clientGUI.initHand(cardId);
+					
+					updateHandSizes();
+										
 				} else if (data[0].equals("dealt")) {
-					clientGUI.updateHandSize(hand.size(), false);
-					clientGUI.updateHandSize(26, true);
+					
+					oppHandSize = 26;
+					updateHandSizes();
+					
 				} else if (data[0].equals("turn")) {
+					
 					clientGUI.enableTurn();
+				
+				} else if (data[0].equals("oppPlayed")) {
+					
+					clientGUI.putCard(data[1], Side.LEFT);
+					oppHandSize--;
+	
+				} else if (data[0].equals("won")) {
+					
+					data = data[1].split("(?<=\\d)(?=\\D)");
+					
+					String cardId = data[0] + data[1] + ".png";
+					
+					hand.addCard(data[0] + data[1]);
+					
+					oppHandSize--;
+					updateHandSizes();
+					
+				} else if (data[0].equals("lost")) {
+					
+					oppHandSize += 2;
+					updateHandSizes();
+					
 				} else if (data[0].equals("serverStop")) {
+					
 					clientGUI.showDialog("The server has been stopped and you were kicked.");
 					System.exit(0);
+					
 				}
 			}
 		} catch(IOException error) {
-			System.out.print(error);
+			System.out.println(error);
 		}
 	}
 	
 	public boolean checkIfTurn() {
 		return isTurn;
 	}
-	
-	public void sendBS() {
-		out.println("bsbs");
-	}
 
 	public void setUsername(String username) {
 	    out.println("name " + username);
 	}
 	
-	public void submit(ClickableCard card) {
+	public void playCard() {
+		ArrayList<String> card = hand.drawTop(1);
+		System.out.println(card.toString());
+		String cardValue = card.get(0);
 		
+		clientGUI.putCard(cardValue, Side.RIGHT);
+		
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		out.println("play " + cardValue);
+		updateHandSize();
+		
+	}
+	
+	public void clearPlayCards() {
+		clientGUI.clearCards();
+	}
+	
+	public void updateHandSizes() {
+		clientGUI.updateHandSize(hand.size(), false);
+		clientGUI.updateHandSize(52 - hand.size(), true);
+	}
+	
+	public void updateHandSize() {
+		clientGUI.updateHandSize(hand.size(), false);
+	}
+	
+	public void updateOpponentHandSize(int amount) {
+		clientGUI.updateHandSize(amount, true);
 	}
 	
 	/*
